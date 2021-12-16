17d904fb354d47488d3960a777c74fdc2d1c5c30
Laura Savickaite <laura.savickaite@laplateforme.io>
2021-12-16 11:08:48 +0100

    register réussi

--- user.php
+++ user.php
@@ -1,28 +1,47 @@
 <?php
 
-require('bdd_connect.php');
+
 
 class User {
     private $id;
     public $login;
+    public $password;
     public $email;
     public $firstname;
     public $lastname;
 
-    function __construct($id, $login, $email, $firstname, $lastname){
-        $this->id = $id;
+    public function __construct(){
+        // $this->login = $login;
+        // $this->password = $password;
+        // $this->email = $email;
+        // $this->firstname = $firstname;
+        // $this->lastname = $lastname;
+    }
+
+
+    public function register($login, $password, $email, $firstname, $lastname){
         $this->login = $login;
+        $this->password = $password;
         $this->email = $email;
         $this->firstname = $firstname;
         $this->lastname = $lastname;
+
+        require('bdd_connect.php');
+
+        $querySql=mysqli_query($connect, "INSERT INTO utilisateurs SET login='$login', password='$password', email='$email', firstname='$firstname', lastname='$lastname'");
+
     }
 }
 
-$User1= new User("1", "bg", "bg@gmail.com", "bb", "gg");
-var_dump($User1);
-print($User1->login);
-
+// $User1= new User("bg","bg","bg@gmail.com", "bb", "gg");
+$User1= new User();
+// var_dump($User1);
+// print($User1->login);
 // on ne peut print quune seule chose :: comme le foreach tableau de ton sqli, si tu veux print plusieurs trucs à chaque fois il faut un nouveau print/echo
+
+$User1 -> register("bg","bg","bg@gmail.com", "bb", "gg");
+// si je veux utiliser la fonction register, il faut que je créé lobjet dabord sinon : quoi envoyer ??? les paramètres de mon construct peuvent être vides, seulement il faut TOUT DE MEME construire un objet comme vu au-dessous, sinon décommenter les this dans construct, rajouter les paramètres dans la fonctions et décommenter le second $User1 new
+
 ?>
 
 
