366ac2f8524325fa7c85e6962db85dfe7d863c32
Laura Savickaite <laura.savickaite@laplateforme.io>
2021-12-20 12:31:27 +0100

    ajout des comments explicatifs sur la procédure à suivre pour les fetch)

--- user-pdo.php
+++ user-pdo.php
@@ -59,7 +59,6 @@ if (isset($_POST['connexion'])) {
                 echo "ok";    
 
                 $_SESSION['login']=$login;
-                var_dump($_SESSION['login']);
             }else {
                 echo "pas ok";
             }
@@ -89,7 +88,54 @@ if(isset($_POST['suppression'])){
 // UPDATE
 
 if(isset($_POST['update'])){
-    
+    $pdo=new PDO($dsn, $user, $password);
+    session_start();
+
+    $sth=$pdo -> prepare("UPDATE `utilisateurs` SET `login`='".$_POST['ulogin']."',`password`='".$_POST['umdp']."',`email`='".$_POST['uemail']."',`firstname`=''".$_POST['ufirstname']."'',`lastname`=''".$_POST['ulastname']."'' WHERE login='".$_SESSION['login']."'");
+    $sth -> execute();
+
+    $_SESSION['login']=$_POST['ulogin'];
+    $_SESSION['email']=$_POST['uemail'];
+    $_SESSION['firstname']=$_POST['ufirstname'];
+    $_SESSION['lastname']=$_POST['ulastname'];
+}
+
+// INFO
+
+if(isset($_SESSION["login"])){
+    $pdo=new PDO($dsn, $user, $password);
+    // $pdo -> setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_OBJ);
+
+    $sth=$pdo -> prepare("SELECT * FROM `utilisateurs` WHERE login='".$_SESSION['login']."'");
+    $sth -> execute();
+
+    // var_dump($sth);
+
+    //le fetch nécessite un mode préétabli, ex FETCH_ASSOC etc...on peut l'établir en amont disant que tous les fetch seront ainsi ($pdo -> setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_OBJ);) OU le définir ainsi suivant : 
+    // (also attention aux print)
+
+    // en array il faudra ensuite faire un foreach si on veut echo (soit entièrement soit undividuellement) OU changer le print_r en print et rajouter une flèche vers ce qu'on veut extraire ainsi que changer le mode, voir ci-dessous
+    // fetch assoc renvoi un array, pour une raison il n'est pas possible de cibler : print $result->login
+    // ce qui n'est pas le cas avec fetch obj
+
+    $result = $sth->fetch(PDO::FETCH_ASSOC);
+    // $results = $sth -> fetchAll(); ne marche que si le mode est défini en amont
+    //et on peut faire un foreach de results ou bien = de result (sans s celui d'en dessous)
+    print_r($result);
+
+    // POUR LE SEUL on peut faire comme ceci : donc changer le mode OU BIEN voir après le dernier print
+
+    $result1 = $sth->fetch(PDO::FETCH_OBJ);
+    print $result1->login;
+    print $result1->email;
+    print $result1->firstname;
+    print $result1->lastname;
+
+    // la technique suivante ne marche que si le fetch mode est défini en amont voir plus haut au début du if avec les deux varibales $pdo EN GROS si on veut ne rien mettre dans les parenthèses de fetch ou fetchALL il faudra toujours définir le fetch mode en amont suite à la création du new pdo et avec une variable de même nom puisque c'est cette variable création qui sera toujours fetch dans ce mode
+
+    // $post = $sth -> fetch();
+    // var_dump($post);
+    // echo $post -> login;
 }
 
 ?>
